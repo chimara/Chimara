da2fd2eb58f6aa8fa4ecf370b40e08f7f52cd866
fliep <fliep@ddfedd41-794f-dd11-ae45-00112f111e67>
2009-06-09 21:48:39 +0000

    Added garglk.c and garglk.h, which is where stubs for Gargoyle's Glk API extensions should go.

---
+++ libchimara/garglk.c
@@ -0,0 +1,6 @@
+#include <libchimara/glk.h>
+
+void 
+garglk_set_reversevideo(glui32 reverse)
+{
+}
---
+++ libchimara/garglk.h
@@ -0,0 +1,6 @@
+#ifndef __GARGLK_H__
+#define __GARGLK_H__
+
+extern void garglk_set_reversevideo(glui32 reverse);
+
+#endif /* __GARGLK_H__ */
