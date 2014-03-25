523811df4155ac5a10b206cb16374d1383e108fc
Philip Chimento <philip.chimento@gmail.com>
2014-03-25 06:16:53 +0000

    Reset Glk style hints at beginning of program
    The Glk program can change the style hints so that different styles
    display differently. These style hints should not carry over into the
    next execution of the Glk program, or even a different Glk program. This
    resets them at the beginning of a Glk program execution.

--- libchimara/style.h
+++ libchimara/style.h
@@ -10,6 +10,7 @@
 G_GNUC_INTERNAL void style_init_textbuffer(GtkTextBuffer *buffer);
 G_GNUC_INTERNAL void style_init_textgrid(GtkTextBuffer *buffer);
 G_GNUC_INTERNAL void style_init(ChimaraGlk *glk);
+G_GNUC_INTERNAL void style_reset_glk(ChimaraGlk *glk);
 G_GNUC_INTERNAL const gchar** style_get_tag_names();
 G_GNUC_INTERNAL void reset_default_styles(ChimaraGlk *glk);
 G_GNUC_INTERNAL GScanner *create_css_file_scanner(void);
