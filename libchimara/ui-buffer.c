chimara/Chimara

commit e385ce1c6295b2a610442e2f28a2eacfd4cd45b1

Fix buffer underflow with finishing line input when none has been given
When finishing line input, we retrieve the entered text and chomp the
newline off the end. However, if the entered text is empty (which happens
when cancelling line input, for example) this would cause a buffer
underflow because we'd write one byte before the beginning of the string.
Fix this by only chomping the newline if the string's length is at least
1.

libchimara/ui-buffer.c
@@ -53,9 +53,9 @@ ui_buffer_finish_line_input(winid_t win, gboolean emit_signal)
 		gtk_text_buffer_delete(window_buffer, &start_iter, &end_iter);
 
 	/* Don't include the newline in the input */
-	char *last_char = inserted_text + strlen(inserted_text) - 1;
-	if(*last_char == '\n')
-		*last_char = '\0';
+	size_t inserted_len = strlen(inserted_text);
+	if (inserted_len >= 1 && inserted_text[inserted_len] == '\n')
+		inserted_text[inserted_len] = '\0';
 
 	int chars_written = ui_textwin_finish_line_input(win, inserted_text, emit_signal);
 	g_free(inserted_text);

commit 8d91a2128c11a27bb1c3d992389cc5537fdfb9d8

Fix string indexing when removing newline
This was a serious regression in e385ce1c6295b2a610442e2f28a2eacfd4cd45b1
in that the parser would never understand any input because it still had a
newline appended to it!

libchimara/ui-buffer.c
@@ -54,8 +54,8 @@ ui_buffer_finish_line_input(winid_t win, gboolean emit_signal)
 
 	/* Don't include the newline in the input */
 	size_t inserted_len = strlen(inserted_text);
-	if (inserted_len >= 1 && inserted_text[inserted_len] == '\n')
-		inserted_text[inserted_len] = '\0';
+	if (inserted_len >= 1 && inserted_text[inserted_len - 1] == '\n')
+		inserted_text[inserted_len - 1] = '\0';
 
 	int chars_written = ui_textwin_finish_line_input(win, inserted_text, emit_signal);
 	g_free(inserted_text);